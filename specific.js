2bb40dc7dc0b657c432f57c85502ced622fed19a
bbranco8 <bbranco.780@gmail.com>
2025-11-18 22:44:10 +0000

    GENRE

--- specific.js
+++ specific.js
@@ -1,11 +1,17 @@
 let books;
 let canvas_width, canvas_height, padding_width, padding_height, bookshelf_width, gap, shelf_height;
 
+let svg, year_buttons_container, genre_buttons_container;
+
+let selected_genres = new Set();
+
+
+// INITIALIZATION
 window.onload = function () {
-    // graph general attributes
-    canvas_width = window.innerWidth * 0.75;
+    // responsive dimensions
+    canvas_width = window.innerWidth * 0.80;
     canvas_height = window.innerHeight * 0.80;
-    padding_width = 40;
+    padding_width = 30;
     padding_height = 60;
     bookshelf_width = canvas_width - padding_width * 2;
     gap = 2;
@@ -16,6 +22,27 @@ window.onload = function () {
         .append('svg')
         .attr('width', canvas_width)
         .attr('height', canvas_height);
+
+    // create years container
+    d3.select('#year_buttons_container').remove(); // remove old container, if there is any
+    year_buttons_container = d3.select('body')
+        .append('div')
+        .attr('id', 'year_buttons_container')
+        .style('position', 'fixed')
+        .style('bottom', '60px')
+        .style('left', (padding_width * 2) + "px");
+
+    // create genre container
+    d3.select('#genre_buttons_container').remove(); // remove old container, if there is any
+    genre_buttons_container = d3.select("body")
+        .append("div")
+        .attr("id", "genre_buttons_container")
+        .style("position", "absolute")
+        .style("top", padding_height + (innerHeight * 0.3) + "px")
+        .style("right", (padding_width * 2) + "px")
+        .style("display", "flex")
+        .style("flex-direction", "column")
+        .style("gap", "10px");
 
 
     books = "livros_dados.csv";
@@ -31,17 +58,18 @@ window.onload = function () {
             publisher: d.Publisher,
             genre: d.Genre
         }
-    }).then(smallMultiples);
+    }).then(process_data);
 }
 
 
-function smallMultiples(data) {
+// DATA PROCESSIGN ---------------------------------------------------------------
+function process_data(data) {
 
     // create intervals of 5 years
     function interval(year) {
         year = +year;
         const start = Math.floor(year / 5) * 5;
-        const end = start + 5;
+        const end = start + 4;
         return { start, end, label: `${start}-${end}` };
     }
 
@@ -50,7 +78,7 @@ function smallMultiples(data) {
 
     // show intervals with at least 10 books
     let valid_intervals = Array.from(group_interval)
-        .filter(([intervalLabel, livros]) => livros.length >= 10);
+        .filter(([label, livros]) => livros.length >= 10);
 
     // sort years cronologically
     valid_intervals.sort((a, b) => {
@@ -60,47 +88,109 @@ function smallMultiples(data) {
     });
     // console.log(valid_intervals);
 
-    // create buttons container
-    d3.select('#year_buttons_container').remove(); // remove old container, if there is any
-    let year_buttons_container = d3.select('body')
-        .append('div')
-        .attr('id', 'buttons-container')
-        .style('position', 'fixed')
-        .style('bottom', '60px')
-        .style('left', '90px')
-
-    // create buttons for each interval of years
-    year_buttons_container.selectAll('button')
-        .data(valid_intervals)
-        .enter()
-        .append('button')
-        .text(d => d[0])  // text of the button = interval of years
-        .style('margin-right', '-2px')
-        .style('padding', '10px')
-        .style('background-color', 'white')
-        .on('click', (event, d) => {
-            // clear previous charts
-            svg.selectAll("*").remove();
-
-            // call fuction to draw the selected interval
-            draw_interval(d[1]);
-        });
+    // create year buttons
+    create_year_buttons(valid_intervals);
 
     // by default, it shows the first interval
     draw_interval(valid_intervals[0][1]);
 
-    // função para desenhar livros do intervalo selecionado
+
+    // BUTTONS FOR YEAR INTERVAL ----------------------------------------------------------
+    function create_year_buttons(valid_intervals) {
+        // create buttons for each year interval
+        year_buttons_container.selectAll('button')
+            .data(valid_intervals)
+            .enter()
+            .append('button')
+            .text(d => d[0])  // text of the button = year interval
+            .style('margin-right', '-2px')
+            .style('padding', '10px')
+            .style('width', `${(canvas_width - padding_width) / valid_intervals.length}px`)
+            .style('background-color', 'white')
+            .on('click', (event, d) => {
+                // clear previous charts
+                svg.selectAll("*").remove();
+
+                // call fuction to draw the selected interval
+                draw_interval(d[1]);
+            });
+    }
+
+
+    // DRAW THE YEAR INTERVAL ---------------------------------------------------------------
     function draw_interval(selected_interval) {
+        // remove books with 0 rating    
+        let filtered_books = selected_interval.filter(d => d.rating > 0);
 
-        // obter todos os gêneros únicos
-        let unique_genres = Array.from(new Set(selected_interval.map(d => d.genre)));
-        console.log(unique_genres);
+        // count how many books exists by genre
+        let genre_counts = d3.rollup(
+            filtered_books,
+            v => v.length,
+            d => d.genre
+        );
 
-        // criar escala de cores para os gêneros
+        // get genres with 2 or more books
+        let valid_genres = Array.from(genre_counts)
+            .filter(([genre, count]) => count >= 2)
+            .map(([genre]) => genre);
+        filtered_books = filtered_books.filter(d => valid_genres.includes(d.genre));
+
+        // create a color scale for genres
         let color_scale = d3.scaleOrdinal()
-            .domain(unique_genres)
+            .domain(valid_genres)
             .range(d3.schemeSet2);
 
+
+        // create genre buttons
+        create_genre_buttons(valid_genres, filtered_books, color_scale);
+
+        // draw all the books in this interval
+        draw_books(filtered_books, color_scale);
+    }
+
+
+    // DRAW THE GENRE BUTTONS ---------------------------------------------------------------
+    function create_genre_buttons(valid_genres, filtered_books, color_scale) {
+        genre_buttons_container.selectAll("*").remove();
+
+        genre_buttons_container.selectAll("button")
+            .data(valid_genres)
+            .enter()
+            .append("button")
+            .text(d => d)
+            .style("padding", "10px")
+            .style("margin-right", "8px")
+            .style("cursor", "pointer")
+            .style("border", "none")
+            .style("color", "white")
+            .style('width', `${(window.innerWidth * 0.15) - padding_width}px`)
+            .style("background-color", d => color_scale(d))
+            .style("opacity", 1)
+            .on("click", function (event, genre) {
+                // alternate selected genre
+                if (selected_genres.has(genre)) {
+                    selected_genres.delete(genre);
+                } else {
+                    selected_genres.add(genre);
+                }
+
+                // button feedback
+                d3.select(this).style("opacity", selected_genres.has(genre) ? 0.6 : 1);
+
+                // update books visibility
+                svg.selectAll("rect")
+                    .style("opacity", d => {
+                        return (
+                            selected_genres.size === 0 ||
+                            selected_genres.has(d.genre)
+                        ) ? 1 : 0;   // it only shows the selected ones
+                    });
+            });
+    }
+
+
+    // DRAW THE BOOKS ---------------------------------------------------------------
+    function draw_books(book_data, color_scale) {
         // scale book height according to its rating
         const height_scale = d3.scaleLinear()
             .domain([1, 5])
@@ -115,22 +205,16 @@ function smallMultiples(data) {
             else return 50; // more than 800
         }
 
-        // create a group to organize its position
-        let book_group = svg.append("g")
-            .attr("transform", `translate(${padding_width}, ${padding_height})`);
-
-        let filtered_books = selected_interval.filter(d => d.rating > 0);
-
         // calculate positions
         let x_positions = [];
         let y_positions = [];
         let current_x = padding_width;
         let current_y = padding_height;
 
-        filtered_books.forEach(d => {
+        book_data.forEach(d => {
             let w = width_scale(d.pages);
 
-            // se passar do limite, quebra linha
+            // if over the limit, break the line.
             if (current_x + w > padding_width + bookshelf_width) {
                 current_x = padding_width;
                 current_y += shelf_height;
@@ -142,9 +226,11 @@ function smallMultiples(data) {
             current_x += w + 5;
         });
 
-        // draw books
+        let book_group = svg.append("g")
+            .attr("transform", `translate(${padding_width}, ${padding_height})`);
+
         book_group.selectAll('rect')
-            .data(filtered_books)  // pega os livros do primeiro intervalo
+            .data(book_data.map((d, i) => ({ ...d, id: i })))
             .enter()
             .append('rect')
             .attr('x', (d, i) => x_positions[i])
@@ -153,5 +239,5 @@ function smallMultiples(data) {
             .attr('height', d => height_scale(Math.floor(d.rating)))
             .attr('fill', d => color_scale(d.genre));
     }
-
 }
+
