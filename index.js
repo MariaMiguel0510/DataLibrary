2adc2da2a01446940c446e7fb28fb20b9e333c18
MariaMiguel0510 <mariammpedro04@gmail.com>
2025-11-23 17:54:15 +0000

    Homepage
    Criei a homepage e os livros
    Criei o contexto da info
    Coloquei o specific dentro do contentor do livro specific

---
+++ index.js
@@ -0,0 +1,120 @@
+import { initializeBooksViz } from './specific.js';
+
+let edition_container, edition_spine, edition_label;
+let info_container, info_spine, info_label;
+let specific_container, specific_spine, specific_label;
+let open = false; //inicialmente todos os livros estão fechados
+
+//INITIALIZATION
+window.onload = function () {
+
+    //EDITION BOOK
+    edition_container = container(d3.select('main'), 'div', 'calc(100vw - 163px)');
+    edition_spine = spine(edition_container, 'div', '80px', '90vh', '-105px', '1', 'auto', true);
+    edition_label = label(edition_spine, 'h3', '2025 EDITION');
+
+    //INFO BOOK
+    info_container = container(d3.select('main'), 'div', 'calc(100vw - 143px)');
+    info_spine = spine(info_container, 'div', '60px', '110vh', '0', '2', 'pointer', false);
+    info_label = label(info_spine, 'h3', 'INFO');
+    mouse_effect(info_spine, '#8CE8FB', 'white');//mouseover fill effect
+    //OPENING TOGGLE INFO
+    toggle(info_spine, [info_container, edition_container], ['calc(100vw - 143px)', 'calc(100vw - 163px)'], ['-3px', '-23px']);
+    //SHOWS CONTEXT
+    //seleciona o template do html e insere-o dentro do info_container
+    let template = document.getElementById('context_template');
+    let info_text = template.content.cloneNode(true);
+    info_container.node().appendChild(info_text);
+
+    //SPECIFIC BOOK
+    specific_container = container(d3.select('main'), 'div', 'calc(100vw - 83px)');
+    specific_spine = spine(specific_container, 'div', '80px', '110vh', '0', '2', 'pointer', false);
+    specific_label = label(specific_spine, 'h3', 'SPECIFIC');
+    initializeBooksViz(specific_container, 'livros_dados.csv');
+    mouse_effect(specific_spine, '#B79FE9', 'white');//mouseover fill effect
+    //OPENING TOGGLE INFO
+    toggle(specific_spine, [info_container, edition_container, specific_container], ['calc(100vw - 143px)', 'calc(100vw - 163px)', 'calc(100vw - 83px)'], ['-63px', '-83px', '-3px']);
+
+
+    //BOOKS CONTAINERS
+    //local selecionado, o que cria, deslocamento
+    function container(selected, place, move) {
+        return selected
+            .append(place)
+            .style('width', '100vw')
+            .style('height', '100vh')
+            .style('left', move)
+            .style('position', 'absolute')
+            .style('bottom', '-20px')
+            .style('background-color', 'white')
+            .style('transition', 'left 0.9s');
+    }
+
+    //BOOKS SPINES
+    //local selecionado, o que cria, largura, altura, deslocamento, z-index, cursor, é ou não animado
+    function spine(selected, place, larg, alt, move, index, rato, animacao) {
+
+        let spine = selected
+            .append(place)
+            .style('width', larg)
+            .style('height', alt)
+            .style('left', move)
+            .style('z-index', index)
+            .style('position', 'absolute')
+            .style('bottom', '0px')
+            .style('border', '3px solid black')
+            .style('background-color', 'white')
+            .style('cursor', rato);
+
+        if (animacao == true) {
+            spine
+                .style('transition', 'right 0.9s')
+                .style('transform', 'rotate(9deg)');
+        }
+        return spine;
+    }
+
+    //BOOKS LABELS
+    function label(selected, place, texto) {
+        return selected
+            .append(place)
+            .text(texto)
+            .style('font-size', '25px')
+            .style('font-weight', '600')
+            .style('margin', '0')
+            .style('bottom', '70px')
+            .style('width', '100%')
+            .style('writing-mode', 'vertical-lr')
+            .style('display', 'flex')
+            .style('justify-content', 'center')
+            .style('align-items', 'center')
+            .style('transform', 'scale(-1,-1)')
+            .style('transform-origin', 'center');
+    }
+
+    //OPENING TOGGLE
+    //elemento de click, contentores a mover, valores pos abertura, valores pos inicial)
+    function toggle(spine, containers, openValues, closedValues) {
+        spine.on('click', function () {
+            for (let i = 0; i < containers.length; i++) {//para todos os contentores existentes
+                if (open) { //se estiver fechado
+                    containers[i].style('left', openValues[i]);
+                } else {//se estiver aberto
+                    containers[i].style('left', closedValues[i]);
+                }
+            }
+            open = !open; //inverte movimento
+        });
+    }
+
+    //MOUSE OVER FILL EFFECT
+    function mouse_effect(spine, hoverColor, normalColor) {
+        spine
+            .on('mouseover', function () {
+                d3.select(this).style('background-color', hoverColor);
+            })
+            .on('mouseout', function () {
+                d3.select(this).style('background-color', normalColor);
+            });
+    }
+}
--- specific.js
+++ specific.js
@@ -1,37 +1,40 @@
-let books;
-let canvas_width, canvas_height, padding_width, padding_height, bookshelf_width, gap, shelf_height;
-let svg, year_buttons_container, genre_buttons_container, arrow_up, arrow_down;
-let selected_genres = new Set();
+export function initializeBooksViz(containerSelector, csvFile) {
+
+    let books;
+    let canvas_width, canvas_height, padding_width, padding_height, bookshelf_width, gap, shelf_height;
+    let svg, year_buttons_container, genre_buttons_container, arrow_up, arrow_down;
+    let selected_genres = new Set();
 
 
-// INITIALIZATION
-window.onload = function () {
+    // INITIALIZATION
     // responsive dimensions
-    canvas_width = window.innerWidth * 0.80;
-    canvas_height = window.innerHeight * 0.80;
     padding_width = 30;
     padding_height = 60;
+
+    canvas_width = containerSelector.node().clientWidth - padding_width * 2;
+    canvas_height = containerSelector.node().clientHeight - padding_height * 2;
+
     bookshelf_width = canvas_width - padding_width * 2;
     gap = 2;
     shelf_height = 80;
 
     // new svg element
-    svg = d3.select('body')
+    svg = containerSelector
         .append('svg')
         .attr('width', canvas_width)
         .attr('height', canvas_height);
 
     // create years container
     d3.select('#year_buttons_container').remove(); // remove old container, if there is any
-    year_buttons_container = d3.select('body')
+    year_buttons_container = containerSelector
         .append('div')
         .attr('id', 'year_buttons_container')
-        .style('position', 'fixed')
+        .style('position', 'absolute')
         .style('bottom', '60px')
         .style('left', (padding_width * 2) + "px");
 
     // lateral container
-    genre_nav = d3.select("body")
+    let genre_nav = containerSelector
         .append("div")
         .attr("id", "genre_nav")
         .style("position", "absolute")
@@ -69,7 +72,7 @@ window.onload = function () {
         .style("cursor", "pointer");
 
 
-    books = "livros_dados.csv";
+    books = csvFile;
 
     d3.csv(books, d => {
         return {
@@ -83,217 +86,217 @@ window.onload = function () {
             genre: d.Genre
         }
     }).then(process_data);
-}
 
 
-// DATA PROCESSIGN ---------------------------------------------------------------
-function process_data(data) {
 
-    // create intervals of 5 years
-    function interval(year) {
-        year = +year;
-        const start = Math.floor(year / 5) * 5;
-        const end = start + 4;
-        return { start, end, label: `${start}-${end}` };
-    }
+    // DATA PROCESSIGN ---------------------------------------------------------------
+    function process_data(data) {
 
-    // group books by intervals
-    let group_interval = d3.group(data, d => interval(d.date).label);
+        // create intervals of 5 years
+        function interval(year) {
+            year = +year;
+            const start = Math.floor(year / 5) * 5;
+            const end = start + 4;
+            return { start, end, label: `${start}-${end}` };
+        }
 
-    // show intervals with at least 10 books
-    let valid_intervals = Array.from(group_interval)
-        .filter(([label, livros]) => livros.length >= 10);
+        // group books by intervals
+        let group_interval = d3.group(data, d => interval(d.date).label);
 
-    // sort years cronologically
-    valid_intervals.sort((a, b) => {
-        const startA = +a[0].split("-")[0];
-        const startB = +b[0].split("-")[0];
-        return d3.ascending(startA, startB);
-    });
-    // console.log(valid_intervals);
+        // show intervals with at least 10 books
+        let valid_intervals = Array.from(group_interval)
+            .filter(([label, livros]) => livros.length >= 10);
 
-    // create year buttons
-    create_year_buttons(valid_intervals);
+        // sort years cronologically
+        valid_intervals.sort((a, b) => {
+            const startA = +a[0].split("-")[0];
+            const startB = +b[0].split("-")[0];
+            return d3.ascending(startA, startB);
+        });
+        // console.log(valid_intervals);
 
-    // by default, it shows the first interval
-    draw_interval(valid_intervals[0][1]);
+        // create year buttons
+        create_year_buttons(valid_intervals);
 
-
-    // BUTTONS FOR YEAR INTERVAL ----------------------------------------------------------
-    function create_year_buttons(valid_intervals) {
-        // create buttons for each year interval
-        year_buttons_container.selectAll('button')
-            .data(valid_intervals)
-            .enter()
-            .append('button')
-            .text(d => d[0])  // text of the button = year interval
-            .style('margin-right', '-2px')
-            .style('padding', '10px')
-            .style('width', `${(canvas_width - padding_width) / valid_intervals.length}px`)
-            .style('background-color', 'white')
-            .on('click', (event, d) => {
-                // clear previous charts
-                svg.selectAll("*").remove();
-
-                // call fuction to draw the selected interval
-                draw_interval(d[1]);
-            });
-    }
+        // by default, it shows the first interval
+        draw_interval(valid_intervals[0][1]);
 
 
-    // DRAW THE YEAR INTERVAL ---------------------------------------------------------------
-    function draw_interval(selected_interval) {
-        // remove books with 0 rating    
-        let filtered_books = selected_interval.filter(d => d.rating > 0);
-
-        // count how many books exists by genre
-        let genre_counts = d3.rollup(
-            filtered_books,
-            v => v.length,
-            d => d.genre
-        );
-
-        // get genres with 2 or more books
-        let valid_genres = Array.from(genre_counts)
-            .filter(([genre, count]) => count >= 2)
-            .map(([genre]) => genre);
-        filtered_books = filtered_books.filter(d => valid_genres.includes(d.genre));
-
-        let genre_colors = ["#B62123", "#ED536D", "#E47476", "#F36D14", "#FF9D52", 
-        "#F3B186", "#D6A315", "#FFCB3E", "#FFDF88", "#40755E", "#B7B427",
-        "#CCE193", "#334FD7", "#6579C7", "#BFD3EB", "#82419D", "#BD73C3",
-        "#E4B1FF", "#FB3E96", "#FD85B3", "#FFCAD1", "#37B6E5", "#7FDCFD"];
-
-        let color_scale = d3.scaleOrdinal()
-            .domain(valid_genres)
-            .range(genre_colors.slice(0, valid_genres.length));
-
-
-        // create genre buttons
-        create_genre_buttons(valid_genres, filtered_books, color_scale);
-
-        // draw all the books in this interval
-        draw_books(filtered_books, color_scale);
-    }
-
-
-    // DRAW THE GENRE BUTTONS ---------------------------------------------------------------
-    function create_genre_buttons(valid_genres, filtered_books, color_scale) {
-
-        let page_size = Math.min(6, valid_genres.length); // if there's 6/+ = 6; if there's 4 = 4
-        let start_index = 0; // initial position
-
-        function render_page() {
-            genre_buttons_container.selectAll("*").remove();
-
-            let page_genres = [];
-
-            for (let i = 0; i < page_size; i++) {
-                // circular logic
-                let index = (start_index + i) % valid_genres.length;
-                page_genres.push(valid_genres[index]);
-            }
-
-            genre_buttons_container.selectAll("button")
-                .data(page_genres)
+        // BUTTONS FOR YEAR INTERVAL ----------------------------------------------------------
+        function create_year_buttons(valid_intervals) {
+            // create buttons for each year interval
+            year_buttons_container.selectAll('button')
+                .data(valid_intervals)
                 .enter()
-                .append("button")
-                .text(d => d)
-                .style("padding", "10px")
-                .style('width', `${(window.innerWidth * 0.15) - padding_width}px`)
-                .style("cursor", "pointer")
-                .style("border", "none")
-                .style("color", "black")
-                .style("background-color", d => color_scale(d))
-                .style("opacity", 1)
-                .on("click", function (event, genre) {
-                    // alternate selected genre
-                    if (selected_genres.has(genre)) {
-                        selected_genres.delete(genre);
-                    } else {
-                        selected_genres.add(genre);
-                    }
+                .append('button')
+                .text(d => d[0])  // text of the button = year interval
+                .style('margin-right', '-2px')
+                .style('padding', '10px')
+                .style('width', `${(canvas_width - padding_width) / valid_intervals.length}px`)
+                .style('background-color', 'white')
+                .on('click', (event, d) => {
+                    // clear previous charts
+                    svg.selectAll("*").remove();
 
-                    // button feedback
-                    d3.select(this).style("opacity", selected_genres.has(genre) ? 0.6 : 1);
-
-                    // update books visibility
-                    svg.selectAll("rect")
-                        .style("opacity", d => {
-                            return (
-                                selected_genres.size === 0 ||
-                                selected_genres.has(d.genre)
-                            ) ? 1 : 0;   // it only shows the selected ones
-                        });
+                    // call fuction to draw the selected interval
+                    draw_interval(d[1]);
                 });
         }
 
-        // ARROW EVENTS  ---------------------------------------------
-        arrow_up.on("click", () => {
-            start_index = (start_index - 1 + valid_genres.length) % valid_genres.length;
-            render_page();
-        });
 
-        arrow_down.on("click", () => {
-            start_index = (start_index + 1) % valid_genres.length;
-            render_page();
-        });
+        // DRAW THE YEAR INTERVAL ---------------------------------------------------------------
+        function draw_interval(selected_interval) {
+            // remove books with 0 rating    
+            let filtered_books = selected_interval.filter(d => d.rating > 0);
+
+            // count how many books exists by genre
+            let genre_counts = d3.rollup(
+                filtered_books,
+                v => v.length,
+                d => d.genre
+            );
+
+            // get genres with 2 or more books
+            let valid_genres = Array.from(genre_counts)
+                .filter(([genre, count]) => count >= 2)
+                .map(([genre]) => genre);
+            filtered_books = filtered_books.filter(d => valid_genres.includes(d.genre));
+
+            let genre_colors = ["#B62123", "#ED536D", "#E47476", "#F36D14", "#FF9D52",
+                "#F3B186", "#D6A315", "#FFCB3E", "#FFDF88", "#40755E", "#B7B427",
+                "#CCE193", "#334FD7", "#6579C7", "#BFD3EB", "#82419D", "#BD73C3",
+                "#E4B1FF", "#FB3E96", "#FD85B3", "#FFCAD1", "#37B6E5", "#7FDCFD"];
+
+            let color_scale = d3.scaleOrdinal()
+                .domain(valid_genres)
+                .range(genre_colors.slice(0, valid_genres.length));
 
 
-        // first render
-        render_page();
-    }
+            // create genre buttons
+            create_genre_buttons(valid_genres, filtered_books, color_scale);
 
-
-    // DRAW THE BOOKS ---------------------------------------------------------------
-    function draw_books(book_data, color_scale) {
-        // scale book height according to its rating
-        const height_scale = d3.scaleLinear()
-            .domain([1, 5])
-            .range([10, 70]);
-
-        // scale book width according to its pages
-        function width_scale(pages) {
-            if (pages <= 200) return 15;
-            else if (pages <= 400) return 20;
-            else if (pages <= 600) return 30;
-            else if (pages <= 800) return 40;
-            else return 50; // more than 800
+            // draw all the books in this interval
+            draw_books(filtered_books, color_scale);
         }
 
-        // calculate positions
-        let x_positions = [];
-        let y_positions = [];
-        let current_x = padding_width;
-        let current_y = padding_height;
 
-        book_data.forEach(d => {
-            let w = width_scale(d.pages);
+        // DRAW THE GENRE BUTTONS ---------------------------------------------------------------
+        function create_genre_buttons(valid_genres, filtered_books, color_scale) {
 
-            // if over the limit, break the line.
-            if (current_x + w > padding_width + bookshelf_width) {
-                current_x = padding_width;
-                current_y += shelf_height;
+            let page_size = Math.min(6, valid_genres.length); // if there's 6/+ = 6; if there's 4 = 4
+            let start_index = 0; // initial position
+
+            function render_page() {
+                genre_buttons_container.selectAll("*").remove();
+
+                let page_genres = [];
+
+                for (let i = 0; i < page_size; i++) {
+                    // circular logic
+                    let index = (start_index + i) % valid_genres.length;
+                    page_genres.push(valid_genres[index]);
+                }
+
+                genre_buttons_container.selectAll("button")
+                    .data(page_genres)
+                    .enter()
+                    .append("button")
+                    .text(d => d)
+                    .style("padding", "10px")
+                    .style('width', `${(window.innerWidth * 0.15) - padding_width}px`)
+                    .style("cursor", "pointer")
+                    .style("border", "none")
+                    .style("color", "black")
+                    .style("background-color", d => color_scale(d))
+                    .style("opacity", 1)
+                    .on("click", function (event, genre) {
+                        // alternate selected genre
+                        if (selected_genres.has(genre)) {
+                            selected_genres.delete(genre);
+                        } else {
+                            selected_genres.add(genre);
+                        }
+
+                        // button feedback
+                        d3.select(this).style("opacity", selected_genres.has(genre) ? 0.6 : 1);
+
+                        // update books visibility
+                        svg.selectAll("rect")
+                            .style("opacity", d => {
+                                return (
+                                    selected_genres.size === 0 ||
+                                    selected_genres.has(d.genre)
+                                ) ? 1 : 0;   // it only shows the selected ones
+                            });
+                    });
             }
 
-            x_positions.push(current_x);
-            y_positions.push(current_y);
+            // ARROW EVENTS  ---------------------------------------------
+            arrow_up.on("click", () => {
+                start_index = (start_index - 1 + valid_genres.length) % valid_genres.length;
+                render_page();
+            });
 
-            current_x += w + 5;
-        });
+            arrow_down.on("click", () => {
+                start_index = (start_index + 1) % valid_genres.length;
+                render_page();
+            });
 
-        let book_group = svg.append("g")
-            .attr("transform", `translate(${padding_width}, ${padding_height})`);
 
-        book_group.selectAll('rect')
-            .data(book_data.map((d, i) => ({ ...d, id: i })))
-            .enter()
-            .append('rect')
-            .attr('x', (d, i) => x_positions[i])
-            .attr('y', (d, i) => y_positions[i] - height_scale(Math.floor(d.rating)))
-            .attr('width', d => width_scale(d.pages))
-            .attr('height', d => height_scale(Math.floor(d.rating)))
-            .attr('fill', d => color_scale(d.genre));
+            // first render
+            render_page();
+        }
+
+
+        // DRAW THE BOOKS ---------------------------------------------------------------
+        function draw_books(book_data, color_scale) {
+            // scale book height according to its rating
+            const height_scale = d3.scaleLinear()
+                .domain([1, 5])
+                .range([10, 70]);
+
+            // scale book width according to its pages
+            function width_scale(pages) {
+                if (pages <= 200) return 15;
+                else if (pages <= 400) return 20;
+                else if (pages <= 600) return 30;
+                else if (pages <= 800) return 40;
+                else return 50; // more than 800
+            }
+
+            // calculate positions
+            let x_positions = [];
+            let y_positions = [];
+            let current_x = padding_width;
+            let current_y = padding_height;
+
+            book_data.forEach(d => {
+                let w = width_scale(d.pages);
+
+                // if over the limit, break the line.
+                if (current_x + w > padding_width + bookshelf_width) {
+                    current_x = padding_width;
+                    current_y += shelf_height;
+                }
+
+                x_positions.push(current_x);
+                y_positions.push(current_y);
+
+                current_x += w + 5;
+            });
+
+            let book_group = svg.append("g")
+                .attr("transform", `translate(${padding_width}, ${padding_height})`);
+
+            book_group.selectAll('rect')
+                .data(book_data.map((d, i) => ({ ...d, id: i })))
+                .enter()
+                .append('rect')
+                .attr('x', (d, i) => x_positions[i])
+                .attr('y', (d, i) => y_positions[i] - height_scale(Math.floor(d.rating)))
+                .attr('width', d => width_scale(d.pages))
+                .attr('height', d => height_scale(Math.floor(d.rating)))
+                .attr('fill', d => color_scale(d.genre));
+        }
     }
 }
-
